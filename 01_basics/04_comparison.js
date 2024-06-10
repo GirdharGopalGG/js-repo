91004741533a381baf72c7ae572f680cecb867a1
Girdhar Gopal <111224545+GirdharGopalGG@users.noreply.github.com>
2024-06-10 01:26:41 +0000

    compare

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);      //converts null to 0
+console.log(null == 0);
+console.log(null >= 0);     //converts null to 0 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+// The === operator in JavaScript is a strict equality operator. It checks for both value equality and type equality. This means that the two operands must be of the SAME TYPE and have the SAME VALUE in order for the expression to evaluate to true.
+
+console.log(2===2);
+console.log("2" === 2);
